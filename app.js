19d5c7c5a68663a7feb2e07cb79aeb61c15dc0b4
B2012105diemkieu <kieuq20021994@gmail.com>
2023-09-20 05:50:20 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 //add router
 const contactsRouter = require("./app/routes/contact.rote");
 
+//add api error
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -17,5 +20,22 @@ app.get("/", (req,res) => {
 //use router
 app.use("/api/contacts", contactsRouter);
 
+//use api error
+app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+//khong co route khop voi yeu cau gui den
+//goi next() chuyen sang middleware xu ly loi
+app.use((req,res,next) => {
+    return next (new ApiError(404, "Resource not found"));
+});
+
+//sua loi trung gian (middleware xu ly loi tap trung)
+//xu ly loi o cac doan code xu ly o cac route (goi next(error))
+app.use((err,req,res,next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
