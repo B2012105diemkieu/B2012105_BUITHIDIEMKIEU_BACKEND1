c8f6f2455a8d51eb2c208057f1ff9dd4b569dbf0
B2012105diemkieu <kieuq20021994@gmail.com>
2023-09-20 04:30:51 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+//const http = require("http");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.json({message: "Welcome to contact book application."});
+});
+
+module.express = app;
